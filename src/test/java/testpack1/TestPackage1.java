13fae73e7a8ff0204ec53ad9af3a2708ffffce67
HTIC1905 <karthika@hticglobal.com>
2026-02-02 21:10:04 +0530

    test test code

---
+++ src/test/java/testpack1/TestPackage1.java
@@ -0,0 +1,14 @@
+package testpack1;
+
+import org.testng.annotations.Test;
+
+public class TestPackage1 {
+    @Test
+    public void sampleTest1() {
+        System.out.println("Hai 1");
+    }
+
+
+}
+
+
